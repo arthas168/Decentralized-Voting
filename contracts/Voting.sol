092aa73f22758a4a0acfbe850f210b2db73bef2e
arthas168 <you@example.com>
2019-04-03 15:38:43 +0300

    almost done

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -3,30 +3,64 @@ pragma solidity ^0.5.0;
 contract Voting {
    uint256 votesForTrump = 0;
    uint256 votesForHillary = 0;
+   uint start = 0;
+   mapping(address => bool) voted;
 
-   function addHillaryVote() public{
+   address owner;
+
+   constructor() public {
+        owner = msg.sender;
+    }
+
+   modifier OnlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+
+     modifier NotVoted(){
+        require(voted[msg.sender]!=true);
+        _;
+    }
+
+    modifier VoteOngoing{
+        require(now < start + 1 minutes);
+        _;
+    }
+
+    modifier VoteEnded{
+        require(now >= start + 1 minutes);
+        _;
+    }
+
+    modifier VoteStarted{
+        require(start!=0);
+        _;
+    }
+
+   function startVote() OnlyOwner public{
+        start = now;
+   }
+
+   function addHillaryVote() NotVoted VoteStarted VoteOngoing public{
        votesForHillary++;
+       voted[msg.sender] = true;
    }
 
-   function addTrumpVote() public{
+   function addTrumpVote() NotVoted VoteStarted VoteOngoing public {
        votesForTrump++;
+        voted[msg.sender] = true;
    }
 
-   function getTrumpVotes()  public view returns(uint256){
+   function getTrumpVotes() VoteEnded public view returns(uint256) {
        return votesForTrump;
    }
 
-   function getHillaryVotes() public view returns(uint256){
+   function getHillaryVotes() VoteEnded public view  returns(uint256) {
        return votesForHillary;
    }
 
-   function getWinner() public view returns (string memory){
-       if(votesForTrump>votesForHillary){
-          return "trump";
-       }else if(votesForTrump==votesForHillary){
-          return "tie";
-       }else if(votesForTrump<votesForHillary){
-           return "hillary";
-       }
+   function resetVote() OnlyOwner VoteEnded public{
+       votesForHillary = 0;
+       votesForTrump = 0;
    }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,35 @@
 $(document).ready(function () {
-    const ContractAddress = "0x85c13a874d779ab5dC4cF9e4d785085AD9F7E4Ae"; //Ropsten contract address
+    //contract info
+    const ContractAddress = "0x5fb6d44bbe6b5cf76c61eaf6eef2a88911153872"; //Ropsten contract address
 
     const ContractABI = [
         {
+            "constant": true,
+            "inputs": [],
+            "name": "getHillaryVotes",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
             "constant": false,
             "inputs": [],
-            "name": "addHillaryVote",
+            "name": "resetVote",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [],
+            "name": "startVote",
             "outputs": [],
             "payable": false,
             "stateMutability": "nonpayable",
@@ -23,24 +47,10 @@ $(document).ready(function () {
         {
             "constant": false,
             "inputs": [],
-            "name": "resetVote",
+            "name": "addHillaryVote",
             "outputs": [],
             "payable": false,
             "stateMutability": "nonpayable",
-            "type": "function"
-        },
-        {
-            "constant": true,
-            "inputs": [],
-            "name": "getHillaryVotes",
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "payable": false,
-            "stateMutability": "view",
             "type": "function"
         },
         {
@@ -58,56 +68,177 @@ $(document).ready(function () {
             "type": "function"
         },
         {
-            "constant": true,
             "inputs": [],
-            "name": "getWinner",
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "string"
-                }
-            ],
             "payable": false,
-            "stateMutability": "view",
-            "type": "function"
+            "stateMutability": "nonpayable",
+            "type": "constructor"
         }
     ];
 
     let contract = web3.eth.contract(ContractABI).at(ContractAddress);
 
-    //Invoke when voter votes for candidate 1
-    $(".trump-btn").click(function () {
-        contract.addTrumpVote(function(err,res) {});
+    //Hide divs
+    $(".countdown-timer").hide();
+    $(".calculating-result-div").hide();
+    $(".resetting-div").hide();
+
+    //Attach Button Events
+    //Invoke to start the vote
+    $(".start").click(function () {
+        try {
+            contract.startVote(function (err, res) {
+            });
+            $(".start-btn-div").hide();
+            initTimer();
+            $(".countdown-timer").show();
+        } catch (e) {
+            alert("You do not have administrative (contract owner) rights!");
+        }
     });
 
     //Invoke when voter votes for candidate 1
-    $(".reset").click(function () {
-        contract.reset(function(err,res) {});
+    $(".trump-btn").click(function () {
+        try {
+            contract.addTrumpVote(function (err, res) {
+                console.log("voted for djt");
+                console.log(res);
+            });
+        } catch (e) {
+            alert("Voting hasn't started yet!")
+        }
     });
 
     //Invoke when voter votes for candidate 2
     $(".hillary-btn").click(function () {
-        contract.addHillaryVote(function(err,res) {});
+        try {
+            contract.addHillaryVote(function (err, res) {
+                console.log("voted for hrc");
+                console.log(res);
+            });
+        } catch (e) {
+            alert("Voting hasn't started yet!")
+        }
     });
 
-    //Get info about winner
-    $(".result").click(function() {
+    function initTimer() {
+        document.getElementById('timer').innerHTML =
+            "01" + ":" + "20";
+        startTimer();
 
+        function startTimer() {
+            var presentTime = document.getElementById('timer').innerHTML;
+            var timeArray = presentTime.split(/[:]+/);
+            var m = timeArray[0];
+            var s = checkSecond((timeArray[1] - 1));
+            if (s == 59) {
+                m = m - 1
+            }
+            if (m == 0 && s == 0) {
+                console.log("timer finished");
+                calculateResult();
+            }
+
+            document.getElementById('timer').innerHTML =
+                m + ":" + s;
+            setTimeout(startTimer, 1000);
+        }
+
+        function checkSecond(sec) {
+            if (sec < 10 && sec >= 0) {
+                sec = "0" + sec;
+            }
+            if (sec < 0) {
+                sec = "59"
+            }
+            return sec;
+        }
+    }
+
+    function initResltCalculationsTimer() {
+        document.getElementById('calc-timer').innerHTML =
+            "01" + ":" + "00";
+        startTimer();
+
+        function startTimer() {
+            var presentTime = document.getElementById('calc-timer').innerHTML;
+            var timeArray = presentTime.split(/[:]+/);
+            var m = timeArray[0];
+            var s = checkSecond((timeArray[1] - 1));
+            if (s == 59) {
+                m = m - 1
+            }
+            if (m == 0 && s == 0) {
+                console.log("calc timer finished");
+                calculateResult();
+            }
+
+            document.getElementById('calc-timer').innerHTML =
+                m + ":" + s;
+            setTimeout(startTimer, 1000);
+        }
+
+        function checkSecond(sec) {
+            if (sec < 10 && sec >= 0) {
+                sec = "0" + sec;
+            }
+            if (sec < 0) {
+                sec = "59"
+            }
+            return sec;
+        }
+    }
+
+    function calculateResult() {
+        $('.countdown-timer').hide();
+        $('.start-btn-div').hide();
+        $('.calculating-result-div').show();
+        $(".hillary-btn").hide();
+        $(".trump-btn").hide();
+        initResltCalculationsTimer();
+        setTimeout(returnStartButton, 60000);
+    }
+
+
+    function returnStartButton() {
+        getResults();
+        $(".calculating-result-div").hide();
+        $(".countdown-timer").hide();
+        $(".resetting-div").show();
+        resetPoints();
+    }
+
+    async function getResults() {
+        let votesForTrump = 0;
+        let votesForHillary = 0;
         //Get votes of each candidate
-        let hillaryVotes = 0;
-        contract.getHillaryVotes(function(err,res)
-        {
-            hillaryVotes = res;
+        await contract.getTrumpVotes(function (err, res) {
+            votesForTrump = res.c[0];
+            console.log(res.c[0]);
         });
-        let trumpVotes = 0;
-        contract.getTrumpVotes(function(err,res)
-        {
-            trumpVotes = res;
+        console.log(votesForTrump);
+        await contract.getTrumpVotes(function (err, res) {
+            votesForHillary = res.c[0];
+            console.log(res.c[0]);
         });
+        console.log(votesForHillary);
 
-        contract.getWinner(function(err,res)
-        {
-            alert(res + " " + "trump " + trumpVotes + " : " + "hillary " + hillaryVotes);
+        alert("trump " + votesForTrump + " : " + "hillary " + votesForHillary);
+    }
+
+    function resetPoints() {
+        $(".calculating-result-div").hide();
+        contract.resetVote(function (err, res) {
+            console.log("reseted");
+            console.log(res);
         });
-    });
+        setTimeout(showStartButton, 5000);
+    }
+
+    function showStartButton() {
+        $(".calculating-result-div").hide();
+        $(".resetting-div").hide();
+        $(".start-btn-div").show();
+        $(".trump-btn").show();
+        $(".hillary-btn").show();
+    }
 });
